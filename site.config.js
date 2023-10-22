4c4595ce828eefdd532acb7595c1dc3c06cb981e
seojeongrok <sjr06060@naver.com>
2023-10-23 01:23:15 +0900

    fix: build error

--- site.config.js
+++ site.config.js
@@ -3,29 +3,29 @@ const CONFIG = {
   profile: {
     name: "seojeongrok",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
+    role: "AI developer",
+    bio: "dog foot son",
+    email: "sjr06060@beamworks.co.kr",
+    // linkedin: "morethanmin",
     github: "seojeongrok",
-    instagram: "",
+    instagram: "tjwjdrok",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `seojeongrok`,
+      href: "https://github.com/seojeongrok",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "jeongrok's blog",
+    description: "welcome to my blog",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
