acc410265a46bb95fc04c85287322b3a4fa43402
seojeongrok <sjr06060@naver.com>
2023-10-23 01:32:39 +0900

    fix: projects error

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -4,31 +4,31 @@ import { AiFillCodeSandboxCircle } from "react-icons/ai"
 import styled from "@emotion/styled"
 import { Emoji } from "src/components/Emoji"
 
-const ServiceCard: React.FC = () => {
-  if (!CONFIG.projects) return null
-  return (
-    <>
-      <StyledTitle>
-        <Emoji>🌟</Emoji> Service
-      </StyledTitle>
-      <StyledWrapper>
-        {CONFIG.projects.map((project, idx) => (
-          <a
-            key={idx}
-            href={`${project.href}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillCodeSandboxCircle className="icon" />
-            <div className="name">{CONFIG.projects[0].name}</div>
-          </a>
-        ))}
-      </StyledWrapper>
-    </>
-  )
-}
+// const ServiceCard: React.FC = () => {
+  // if (!CONFIG.projects) return null
+  // return (
+  //   <>
+  //     <StyledTitle>
+  //       <Emoji>🌟</Emoji> Service
+  //     </StyledTitle>
+  //     <StyledWrapper>
+  //       {CONFIG.projects.map((project, idx) => (
+  //         <a
+  //           key={idx}
+  //           href={`${project.href}`}
+  //           rel="noreferrer"
+  //           target="_blank"
+  //         >
+  //           <AiFillCodeSandboxCircle className="icon" />
+  //           <div className="name">{CONFIG.projects[0].name}</div>
+  //         </a>
+  //       ))}
+  //     </StyledWrapper>
+  //   </>
+  // )
+// }
 
-export default ServiceCard
+// export default ServiceCard
 
 const StyledTitle = styled.div`
   padding: 0.25rem;
