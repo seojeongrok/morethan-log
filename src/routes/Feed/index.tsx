df6a4fd4177235b59954a45f076d01f7143ca7d8
seojeongrok <sjr06060@naver.com>
2023-10-23 01:33:25 +0900

    fix: projects error

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -7,7 +7,7 @@ import styled from "@emotion/styled"
 import TagList from "./TagList"
 import MobileProfileCard from "./MobileProfileCard"
 import ProfileCard from "./ProfileCard"
-import ServiceCard from "./ServiceCard"
+// import ServiceCard from "./ServiceCard"
 import ContactCard from "./ContactCard"
 import PostList from "./PostList"
 
@@ -47,7 +47,7 @@ const Feed: React.FC<Props> = () => {
         }}
       >
         <ProfileCard />
-        <ServiceCard />
+        {/* <ServiceCard /> */}
         <ContactCard />
         <div className="footer">
           <Footer />
